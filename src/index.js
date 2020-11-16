d299b1840ecf3dfa205944c8bb90b6469c71f0be
Sean Stokes <seanstokestemp1@gmail.com>
2020-11-16 10:46:26 +0000

    useMovie custom hook

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import MovieReviewPage from "./pages/movieReviewPage";
 import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
@@ -5,7 +6,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom"    // CHANGED
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
-import MovieReviewPage from "./pages/movieReviewPage";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -24,10 +25,10 @@ const App = () => {
           </li>
         </ul>
         <Switch>
+        <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
-          <Route path="/reviews/:id" component={MovieReviewPage} />
           <Redirect from="*" to="/" />
         </Switch>
       </div>
