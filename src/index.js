233231518b0b13434cac46019a136b59e0359d1a
Sean Stokes <seanstokestemp1@gmail.com>
2020-11-16 10:32:42 +0000

    Full review page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom"    // CHANGED
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'       // NEW
-
+import MovieReviewPage from "./pages/movieReviewPage";
 const App = () => {
   return (
     <BrowserRouter>
@@ -27,6 +27,7 @@ const App = () => {
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route path="/" component={HomePage} />
+          <Route path="/reviews/:id" component={MovieReviewPage} />
           <Redirect from="*" to="/" />
         </Switch>
       </div>
