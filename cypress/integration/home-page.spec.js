afbb3cfc1a51d6d66cbe475d663fac4b9fb5822e
Sean Stokes <seanstokestemp1@gmail.com>
2020-12-16 20:41:11 +0000

    Testing another way to pass the api key through to the cypress homepage.spec

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -12,9 +12,9 @@ describe("Home Page ", () => {
   before(() => {
     // Get movies from TMDB and store in movies variable.
     cy.request(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
-        "TMDB_KEY"
-      )}&language=en-US&include_adult=false&include_video=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${
+        TMDB_KEY
+      }&language=en-US&include_adult=false&include_video=false&page=1`
     )
       .its("body")    // Take the body of HTTP response from TMDB
       .then((response) => {
